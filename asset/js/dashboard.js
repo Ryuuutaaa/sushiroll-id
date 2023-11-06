b2c1bcb3b80ea6683535342f0beed19d62706f55
= <=>
2023-11-06 23:59:52 +0800

    fixed fiture login logout and design page login and button logout

--- asset/js/dashboard.js
+++ asset/js/dashboard.js
@@ -1,8 +1,8 @@
-window.onload = function () {
-  // Menutup halaman "index"
-  window.opener.close();
-  // alert("Berhasil login");
-};
+// window.onload = function () {
+//   // Menutup halaman "index"
+//   window.opener.close();
+//   // alert("Berhasil login");
+// };
 
 var logoutButton = document.getElementById("btn-logout");
 
@@ -10,22 +10,7 @@ logoutButton.addEventListener("click", function () {
   var confirmLogout = confirm("Apakah Anda yakin ingin logout?");
 
   if (confirmLogout) {
-    window.location.href = "index.html";
-  } else {
+    window.close("dashboard.html");
+    window.open("index.html");
   }
 });
-
-// const alertPlaceholder = document.getElementById("i");
-// const appendAlert = (message, type) => {
-//   const wrapper = document.createElement("div");
-//   wrapper.innerHTML = [`<div class="alert alert-${type} alert-dismissible" role="alert">`, `   <div>${message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', "</div>"].join("");
-
-//   alertPlaceholder.append(wrapper);
-// };
-
-// const alertTrigger = document.getElementById("liveAlertBtn");
-// if (alertTrigger) {
-//   alertTrigger.addEventListener("click", () => {
-//     appendAlert("Nice, you triggered this alert message!", "success");
-//   });
-// }
--- asset/js/index.js
+++ asset/js/index.js
@@ -1,9 +1,12 @@
 function check(form) {
   if (form.userid.value == "admin" && form.pswrd.value == "admin123") {
+    alert("Berhasil login");
     window.close("index.html");
     window.open("dashboard.html");
-    alert("Berhasil login");
-  } else if (form.userid.value.trim() === "" || form.pswrd.value.trim() === "") {
+  } else if (
+    form.userid.value.trim() === "" ||
+    form.pswrd.value.trim() === ""
+  ) {
     alert("Error: Username atau Password harus diisi.");
   } else {
     alert("Error: Username atau Password salah.");
